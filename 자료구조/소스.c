e275109a1ffcb4b413e8fcc848fb4235552610fa
stoooooone <kcy0428@naver.com>
2022-09-13 13:38:14 +0900

    동적 배열 스택

--- 자료구조/소스.c
+++ 자료구조/소스.c
@@ -1,4 +1,4 @@
-//스택
+/*스택
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -54,6 +54,69 @@ int main(void) {
 	printf("%d\n", pop());
 	printf("%d\n", pop());
 	printf("%d\n", pop());
+
+	return 0;
+}
+*/
+
+//동적 배열 스택 프로그램
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_STACK_SIZE 100
+
+typedef int element;
+typedef struct {
+	element* data;
+	int capacity;
+	int top;
+} StackType;
+
+//스택 생성 함수
+void init_stack(StackType* s) {
+	s->top = -1;
+	s->capacity = 1;
+	s->data = (element*)malloc(s->capacity * sizeof(element));
+}
+
+//공백 상태 검출 함수
+int is_empty(StackType* s) {
+	return (s->top == -1);
+}
+
+//포화 상태 검출 함수
+int is_full(StackType* s) {
+	return (s->top == (s->capacity - 1));
+}
+
+//삽입 함수
+void push(StackType* s, element item) {
+	if (is_full(s)) {
+		s->capacity *= 2;
+		s->data = (element*)realloc(s->data, s->capacity * sizeof(element));
+	}
+	s->data[++(s->top)] = item;
+}
+
+//삭제 함수
+element pop(StackType* s) {
+	if (is_empty(s)) {
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else return s->data[(s->top)--];
+}
+
+int main(void) {
+	StackType s;
+	init_stack(&s);
+	push(&s, 1);
+	push(&s, 2);
+	push(&s, 3);
+	printf("%d \n", pop(&s));
+	printf("%d \n", pop(&s));
+	printf("%d \n", pop(&s));
+	free(s.data);
 
 	return 0;
 }
