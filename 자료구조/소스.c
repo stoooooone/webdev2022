571dacde669d101c783f9a6c67b1371804099753
stoooooone <kcy0428@naver.com>
2022-09-14 12:16:17 +0900

    selection sort

--- 자료구조/소스.c
+++ 자료구조/소스.c
@@ -59,6 +59,7 @@ int main(void) {
 }
 */
 
+/*
 //동적 배열 스택 프로그램
 
 #include <stdio.h>
@@ -117,6 +118,51 @@ int main(void) {
 	printf("%d \n", pop(&s));
 	printf("%d \n", pop(&s));
 	free(s.data);
+
+	return 0;
+}
+*/
+
+//선택 정렬
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 10
+#define SWAP(x, y, t) (t=x, x=y, y=t)
+
+int list[MAX_SIZE];
+int n;
+
+void selection_sort(int list[], int n) {
+	int i, j, least, temp;
+	for (i = 0; i < n - 1; i++) {
+		least = i;
+		for (j = i + 1; j < n; j++) {
+			if (list[j] < list[least]) {
+				SWAP(list[j], list[least], temp);
+			}
+		}
+	}
+}
+
+int main(void) {
+	int i;
+	n = MAX_SIZE;
+	srand(time(NULL));
+	for (i = 0; i < n; i++) {
+		list[i] = rand() % 100;
+	}
+
+	for (i = 0; i < n; i++) {
+		printf("%d ", list[i]);
+	}
+	printf("\n");
+
+	selection_sort(list, n);
+	for (i = 0; i < n; i++) {
+		printf("%d ", list[i]);
+	}
+	printf("\n");
 
 	return 0;
 }
