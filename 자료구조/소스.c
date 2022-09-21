3a8c0425dfc52f527f083ae8789a32cb8353009d
stoooooone <kcy0428@naver.com>
2022-09-21 12:46:06 +0900

    shell sort

--- 자료구조/소스.c
+++ 자료구조/소스.c
@@ -169,6 +169,26 @@ void bubble_sort(int list[], int n) {
 	}
 }
 
+//½© Á¤·Ä
+inc_insertion_sort(int list[], int first, int last, int gap) {
+	int i, j, key;
+	for (i = first + gap; i <= last; i = i + gap) {
+		key = list[i];
+		for (j = i - gap; j >= first && key < list[j]; j = j - gap) {
+			list[j + gap] = list[j];
+		}
+		list[j + gap] = key;
+	}
+}
+void shell_sort(int list[], int n) {
+	int i, gap;
+	for (gap = n / 2; gap > 0; gap = gap / 2) {
+		if ((gap % 2) == 0) gap++;
+		for (i = 0; i < gap; i++)
+			inc_insertion_sort(list, i, n - 1, gap);
+	}
+}
+
 int main(void) {
 	int i;
 	n = MAX_SIZE;
@@ -182,7 +202,7 @@ int main(void) {
 	}
 	printf("\n");
 
-	bubble_sort(list, n);
+	shell_sort(list, n);
 	for (i = 0; i < n; i++) {
 		printf("%d ", list[i]);
 	}
