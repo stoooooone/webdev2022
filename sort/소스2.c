7af3737e04bd8de190b28bf9edc55c29493eff22
stoooooone <kcy0428@naver.com>
2022-09-22 23:42:40 +0900

    sort program

--- sort/소스2.c
+++ sort/소스2.c
@@ -1,11 +1,193 @@
+/*스택
+
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_SIZE 20
-#define SWAP(x, y, t) (t=x, x=y, y=t)
 
+#define MAX_STACK_SIZE 100
+typedef int element;
+element stack[MAX_STACK_SIZE];
+int top = -1;
+
+//공백 상태 검출 함수
+int is_empty() {
+	return (top == -1);
+}
+
+//포화 상태 검출 함수
+int is_full() {
+	return (top == MAX_STACK_SIZE - 1);
+}
+
+//삽입 함수
+void push(element item) {
+	if (is_full()) {
+		fprintf(stderr, "스택 포화 에러\n");
+		return;
+	}
+	else stack[++top] = item;
+}
+
+//삭제 함수
+element pop() {
+	if (is_empty()) {
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else {
+		return stack[top--];
+	}
+}
+
+//피크 함수
+element peek() {
+	if (is_empty()) {
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else return stack[top];
+}
+
+int main(void) {
+	push(1);
+	push(2);
+	push(3);
+	printf("%d\n", pop());
+	printf("%d\n", pop());
+	printf("%d\n", pop());
+
+	return 0;
+}
+*/
+
+/*
+//동적 배열 스택 프로그램
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_STACK_SIZE 100
+
+typedef int element;
+typedef struct {
+	element* data;
+	int capacity;
+	int top;
+} StackType;
+
+//스택 생성 함수
+void init_stack(StackType* s) {
+	s->top = -1;
+	s->capacity = 1;
+	s->data = (element*)malloc(s->capacity * sizeof(element));
+}
+
+//공백 상태 검출 함수
+int is_empty(StackType* s) {
+	return (s->top == -1);
+}
+
+//포화 상태 검출 함수
+int is_full(StackType* s) {
+	return (s->top == (s->capacity - 1));
+}
+
+//삽입 함수
+void push(StackType* s, element item) {
+	if (is_full(s)) {
+		s->capacity *= 2;
+		s->data = (element*)realloc(s->data, s->capacity * sizeof(element));
+	}
+	s->data[++(s->top)] = item;
+}
+
+//삭제 함수
+element pop(StackType* s) {
+	if (is_empty(s)) {
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else return s->data[(s->top)--];
+}
+
+int main(void) {
+	StackType s;
+	init_stack(&s);
+	push(&s, 1);
+	push(&s, 2);
+	push(&s, 3);
+	printf("%d \n", pop(&s));
+	printf("%d \n", pop(&s));
+	printf("%d \n", pop(&s));
+	free(s.data);
+
+	return 0;
+}
+*/
+
+//정렬
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 21
+#define SWAP(x, y, t) (t=x, x=y, y=t)
 
 int list[MAX_SIZE];
 int n;
+
+//선택 정렬
+void selection_sort(int list[], int n) {
+	int i, j, least, temp;
+	for (i = 0; i < n - 1; i++) {
+		least = i;
+		for (j = i + 1; j < n; j++) {
+			if (list[j] < list[least]) {
+				SWAP(list[j], list[least], temp);
+			}
+		}
+	}
+}
+
+//삽입 정렬
+void insertion_sort(int list[], int n) {
+	int i, j, key;
+	for (i = 1; i < n; i++) {
+		key = list[i];
+		for (j = i - 1; j >= 0 && list[j] > key; j--) {
+			list[j + 1] = list[j];
+		}
+		list[j + 1] = key;
+	}
+}
+
+//버블 정렬
+void bubble_sort(int list[], int n) {
+	int i, j, temp;
+	for (i = n - 1; i > 0; i--) {
+		for (j = 0; j < i; j++) {
+			if (list[j] > list[j + 1])
+				SWAP(list[j], list[j + 1], temp);
+		}
+	}
+}
+
+//쉘 정렬
+inc_insertion_sort(int list[], int first, int last, int gap) {
+	int i, j, key;
+	for (i = first + gap; i <= last; i = i + gap) {
+		key = list[i];
+		for (j = i - gap; j >= first && key < list[j]; j = j - gap) {
+			list[j + gap] = list[j];
+		}
+		list[j + gap] = key;
+	}
+}
+void shell_sort(int list[], int n) {
+	int i, gap;
+	for (gap = n / 2; gap > 0; gap = gap / 2) {
+		if ((gap % 2) == 0) gap++;
+		for (i = 0; i < gap; i++)
+			inc_insertion_sort(list, i, n - 1, gap);
+	}
+}
 
 //합병 정렬
 int sorted[MAX_SIZE];	//추가 공간이 필요
