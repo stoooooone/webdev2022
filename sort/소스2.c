dde378d89fd9f16fe5ca594c0167ee785d3121d5
stoooooone <kcy0428@naver.com>
2022-09-26 17:33:05 +0900

    quick sort

--- sort/소스2.c
+++ sort/소스2.c
@@ -234,6 +234,36 @@ void merge_sort(int list[], int left, int right) {
 	}
 }
 
+//Äü Á¤·Ä
+int partition(int list[], int left, int right) {
+	int pivot, temp;
+	int low, high;
+
+	low = left;
+	high = right + 1;
+	pivot = list[left];
+	do {
+		do
+			low++;
+		while (list[low] < pivot);
+		do
+			high--;
+		while (list[high] > pivot);
+		if (low < high) SWAP(list[low], list[high], temp);
+	} while (low < high);
+
+	SWAP(list[left], list[high], temp);
+	return high;
+}
+
+void quick_sort(int list[], int left, int right) {
+	if (left < right) {
+		int q = partition(list, left, right);
+		quick_sort(list, left, q - 1);
+		quick_sort(list, q + 1, right);
+	}
+}
+
 int main(void) {
 	int i;
 	n = MAX_SIZE;
@@ -247,7 +277,7 @@ int main(void) {
 	}
 	printf("\n");
 
-	merge_sort(list, 0, MAX_SIZE - 1);
+	quick_sort(list, 0, MAX_SIZE - 1);
 	for (i = 0; i < n; i++) {
 		printf("%d ", list[i]);
 	}
