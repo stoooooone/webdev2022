7a63531485cbb6910793373ffb85efceabdeb3de
stoooooone <kcy0428@naver.com>
2022-09-27 23:54:53 +0900

    queue 하는중

--- 자료구조/소스.c
+++ 자료구조/소스.c
@@ -122,3 +122,71 @@ int main(void) {
 	return 0;
 }
 */
+
+
+/*큐*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_QUEUE_SIZE 5
+
+typedef int element;
+typedef struct {
+	int front;
+	int rear;
+	element data[MAX_QUEUE_SIZE];
+} QueueType;
+
+//오류 함수
+void error(char* message) {
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
+
+void init_queue(QueueType* q) {
+	q->rear = -1;
+	q->front = -1;
+}
+
+void queue_print(QueueType* q) {
+	for (int i = 0; i < MAX_QUEUE_SIZE; i++) {
+		if (i <= q->front || i > q->rear)
+			printf(" | ");
+		else
+			printf("%d | ", q->data[i]);
+	}
+	printf("\n");
+}
+
+int is_full(QueueType* q) {
+
+}
+
+int is_empty(QueueType* q) {
+
+}
+
+void enqueue(QueueType* q, int item) {
+
+}
+
+int dequeue(QueueType* q) {
+
+}
+
+int main(void) {
+	int item = 0;
+	QueueType q;
+
+	init_queue(&q);
+
+	enqueue(&q, 10); queue_print(&q);
+	enqueue(&q, 20); queue_print(&q);
+	enqueue(&q, 30); queue_print(&q);
+
+	item = dequeue(&q); queue_print(&q);
+	item = dequeue(&q); queue_print(&q);
+	item = dequeue(&q); queue_print(&q);
+
+	return 0;
+}
