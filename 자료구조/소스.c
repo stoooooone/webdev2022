7ac021c207859419059560eb5ec6290dfafec3fc
stoooooone <kcy0428@naver.com>
2022-09-30 21:48:13 +0900

    원형큐

--- 자료구조/소스.c
+++ 자료구조/소스.c
@@ -124,7 +124,7 @@ int main(void) {
 */
 
 
-/*큐*/
+/*선형큐
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -203,5 +203,97 @@ int main(void) {
 	item = dequeue(&q); queue_print(&q);
 	item = dequeue(&q); queue_print(&q);
 
+	return 0;
+}
+
+*/
+
+
+/*원형큐*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_QUEUE_SIZE 5
+
+typedef int element;
+typedef struct {
+	int front;
+	int rear;
+	element data[MAX_QUEUE_SIZE];
+} QueueType;
+
+//오류 함수
+void error(char* message) {
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
+
+void init_queue(QueueType* q) {
+	q->front = q->rear = 0;
+}
+
+int is_empty(QueueType* q) {
+	return (q->front == q->rear);
+}
+
+int is_full(QueueType* q) {
+	return ((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
+}
+
+void queue_print(QueueType* q) {
+	printf("QUEUE(front=%d rear=%d) = ", q->front, q->rear);
+	if (!is_empty(q)) {
+		int i = q->front;
+		do {
+			i = (i + 1) % (MAX_QUEUE_SIZE);
+			printf("%d | ", q->data[i]);
+			if (i == q->rear)
+				break;
+		} while (i != q->front);
+	}
+	printf("\n");
+}
+
+void enqueue(QueueType* q, element item) {
+	if (is_full(q))
+		error("큐가 포화상태입니다");
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->data[q->rear] = item;
+}
+
+element dequeue(QueueType* q) {
+	if (is_empty(q))
+		error("큐가 공백상태입니다");
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+	return q->data[q->front];
+}
+
+element peek(QueueType* q) {
+	if (is_empty(q))
+		error("큐가 공백상태입니다");
+	return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
+}
+
+int main(void) {
+	QueueType queue;
+	int element;
+
+	init_queue(&queue);
+	printf("--데이터 추가 단계--\n");
+	while (!is_full(&queue)) {
+		printf("정수를 입력하세요 : ");
+		scanf_s("%d", &element);
+		enqueue(&queue, element);
+		queue_print(&queue);
+	}
+	printf("큐는 포화상태입니다. \n\n");
+
+	printf("--데이터 삭제 단계--\n");
+	while (!is_empty(&queue)) {
+		element = dequeue(&queue);
+		printf("꺼내진 정수 : %d \n", element);
+		queue_print(&queue);
+	}
+	printf("큐는 공백상태입니다\n");
 	return 0;
 }
