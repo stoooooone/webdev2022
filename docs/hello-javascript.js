ea0860decd52fd1b8d102ab896a21c4a7f1d1aba
stoooooone <kcy0428@naver.com>
2022-11-18 15:58:43 +0900

    폴더로 이동

---
+++ docs/hello-javascript.js
@@ -0,0 +1,29 @@
+// console.log('hello')
+// alert(1)
+
+
+// let, const 사용
+let let1 = 1;
+let let2 = 3;
+let1 = 5;
+let2 = 'a';
+//let let1 = 'abc';     재선언은 불가능
+
+let a = "3";
+let b = a + "abc";
+console.log(b)
+let c = 3 * a;
+console.log(c)
+// let boolean1 = !!let(a);
+
+if (!0) {
+    alert('False');
+}
+
+
+const b2 = new Boolean(false);
+
+if (b2) {
+    console.log('b2는 true. 객체입니다')
+}
+
