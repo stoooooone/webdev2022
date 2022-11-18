5cf7ec4a2523bbbcf0d77f3b1744aff8ed8ec23f
stoooooone <kcy0428@naver.com>
2022-11-18 16:13:44 +0900

    11월 18일 수업 & 폴더정리

---
+++ JAVA/JavaPractice/src/main/java/matrixChain.java
@@ -0,0 +1,19 @@
+
+class Matrix {
+	int m[][] = new int[][];
+	
+}
+
+public class matrixChain {
+	
+	public int MatrixChain(int i, int j) {
+		for (int k=0; k<=n; k++) {
+			m[i,j]
+		}
+			
+		
+		return m[1, n];
+	}
+	
+	
+}
--- docs/hello-javascript.js
+++ docs/hello-javascript.js
@@ -9,11 +9,11 @@ let1 = 5;
 let2 = 'a';
 //let let1 = 'abc';     재선언은 불가능
 
-let a = "3";
-let b = a + "abc";
-console.log(b)
-let c = 3 * a;
-console.log(c)
+let a2 = "3";
+let b2 = a2 + "abc";
+console.log(b2)
+let c2 = 3 * a2;
+console.log(c2)
 // let boolean1 = !!let(a);
 
 if (!0) {
@@ -21,9 +21,41 @@ if (!0) {
 }
 
 
-const b2 = new Boolean(false);
+const b3 = new Boolean(false);
 
-if (b2) {
-    console.log('b2는 true. 객체입니다')
+if (b3) {
+    console.log('b3는 true. 객체입니다');
 }
 
+
+// 함수 선언식
+function a() {
+    console.log(1);
+}
+
+// 함수 표현식
+const b = a;
+const c = () => {
+    console.log(2);
+}
+
+// 함수 축약형
+class AClass {
+    fn() {
+        console.log(3);
+    }
+}
+
+// 즉시 실행  IIFE
+(function() {
+    console.log('즉시실행');
+})()
+
+
+if (true) {
+    var globala = 1;
+}
+
+a();
+b();
+c();
