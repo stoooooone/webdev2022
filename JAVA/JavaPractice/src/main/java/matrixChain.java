b6c241307f46270d9adf2223a3f011c29ccaa7e1
stoooooone <kcy0428@naver.com>
2022-11-24 23:37:22 +0900

    JAVA 행렬곱셈순서

--- JAVA/JavaPractice/src/main/java/matrixChain.java
+++ JAVA/JavaPractice/src/main/java/matrixChain.java
@@ -1,19 +1,46 @@
 
-class Matrix {
-	int m[][] = new int[][];
-	
+
+
+
+
+class recursiveMatrixChain {
+	protected int rmchain(int i, int j) {
+		if (i==j) return 0;
+		else {
+			int min=999999999;
+			
+			int temp;
+			temp = Array[]
+			
+			for (int k = i; k<=j-1; k++) {
+				int res = rmchain(i, k) + rmchain(k+1, j) + temp;
+				
+				if (res<min) 
+					min = res;
+			}
+			
+			return min;
+		}
+
+	}
+}
+
+
+class DPMatrixChain {
+	protected int dpmchain(int i, int j) {
+		
+		
+	}
 }
 
 public class matrixChain {
-	
-	public int MatrixChain(int i, int j) {
-		for (int k=0; k<=n; k++) {
-			m[i,j]
-		}
-			
+	public void main(String[] args) {
 		
-		return m[1, n];
+		
+		
+		
+		
+		
+		
 	}
-	
-	
 }
