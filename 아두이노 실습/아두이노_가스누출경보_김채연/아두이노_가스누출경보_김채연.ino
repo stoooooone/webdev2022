fc82c679d63d300f69bfcbcd34a51a1020d43e22
stoooooone <kcy0428@naver.com>
2022-12-16 22:35:58 +0900

    아두이노 가스누출경보 주석 작성

--- 아두이노 실습/아두이노_가스누출경보_김채연/아두이노_가스누출경보_김채연.ino
+++ 아두이노 실습/아두이노_가스누출경보_김채연/아두이노_가스누출경보_김채연.ino
@@ -1,10 +1,10 @@
 // 임베디드 컴퓨팅 기말과제 2021108255 김채연
 
-#define GasPin A0
-int buzzerPin = 8;
-int LED = 13;
+#define GasPin A0   // 가스센서 A0 핀에 연결
+int buzzerPin = 8;   // 부저 8번 핀에 연결
+int LED = 13;       // LED 13번 핀에 연결
 int GasValue;
-int GasThreshold = 500;
+int GasThreshold = 500;   // 가스 농도 임계치
 
 void setup() {
   pinMode(GasPin, INPUT);
@@ -14,11 +14,11 @@ void setup() {
 }
 
 void loop() {
-  GasValue = analogRead(GasPin);
+  GasValue = analogRead(GasPin);  // 가스 센서정보 읽어오기
   Serial.print("GasValue : ");
   Serial.print(GasValue);
 
-  if (GasValue >= GasThreshold) {
+  if (GasValue >= GasThreshold) {   // 가스 농도가 임계치 이상의 값일 때
     digitalWrite(LED, HIGH);
     digitalWrite(buzzerPin, HIGH);
     Serial.println("  Warning!!")
@@ -29,5 +29,5 @@ void loop() {
     Serial.println();
   }
 
-  delay(1000);
+  delay(1000);    // 1초 지연
 }
